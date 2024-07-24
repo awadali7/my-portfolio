aa2cd4b44aa88f9c78d4e7e0c9cb6586ac384a84
awadali <mailtoawadali@gmail.com>
2024-07-24 23:41:24 +0530

    final update

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -4,7 +4,7 @@ import { NextSeo } from 'next-seo';
 import BackButton from '@/common/components/elements/BackButton';
 import Container from '@/common/components/elements/Container';
 import PageHeading from '@/common/components/elements/PageHeading';
-import prisma from '@/common/libs/prisma';
+// import prisma from '@/common/libs/prisma';
 import { ProjectItemProps } from '@/common/types/projects';
 import ProjectDetail from '@/modules/projects/components/ProjectDetail';
 
@@ -13,66 +13,67 @@ interface ProjectsDetailPageProps {
 }
 
 const ProjectsDetailPage: NextPage<ProjectsDetailPageProps> = ({ project }) => {
-  const PAGE_TITLE = project?.title;
-  const PAGE_DESCRIPTION = project?.description;
+  // const PAGE_TITLE = project?.title;
+  // const PAGE_DESCRIPTION = project?.description;
 
-  const canonicalUrl = `https://awad.id/project/${project?.slug}`;
+  // const canonicalUrl = `https://awad.id/project/${project?.slug}`;
 
   return (
-    <>
-      <NextSeo
-        title={`${project?.title} - Project Awad Ali`}
-        description={project?.description}
-        canonical={canonicalUrl}
-        openGraph={{
-          type: 'article',
-          article: {
-            publishedTime: project?.updated_at.toString(),
-            modifiedTime: project?.updated_at.toString(),
-            authors: ['Awad Ali'],
-          },
-          url: canonicalUrl,
-          images: [
-            {
-              url: project?.image,
-            },
-          ],
-          siteName: 'Blog Awad Ali',
-        }}
-      />
-      <Container data-aos='fade-up'>
-        <BackButton url='/projects' />
-        <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
-        <ProjectDetail {...project} />
-      </Container>
-    </>
+    // <>
+    //   <NextSeo
+    //     title={`${project?.title} - Project Awad Ali`}
+    //     description={project?.description}
+    //     canonical={canonicalUrl}
+    //     openGraph={{
+    //       type: 'article',
+    //       article: {
+    //         publishedTime: project?.updated_at.toString(),
+    //         modifiedTime: project?.updated_at.toString(),
+    //         authors: ['Awad Ali'],
+    //       },
+    //       url: canonicalUrl,
+    //       images: [
+    //         {
+    //           url: project?.image,
+    //         },
+    //       ],
+    //       siteName: 'Blog Awad Ali',
+    //     }}
+    //   />
+    //   <Container data-aos='fade-up'>
+    //     <BackButton url='/projects' />
+    //     <PageHeading title={PAGE_TITLE} description={PAGE_DESCRIPTION} />
+    //     <ProjectDetail {...project} />
+    //   </Container>
+    // </>
+    <></>
   );
 };
 
 export default ProjectsDetailPage;
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const response = await prisma.projects.findUnique({
-    where: {
-      slug: String(params?.slug),
-    },
-  });
+// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+//   const response = await prisma.projects.findUnique({
+//     where: {
+//       slug: String(params?.slug),
+//     },
+//   });
 
-  if (response === null) {
-    return {
-      redirect: {
-        destination: '/404',
-        permanent: false,
-      },
-    };
-  }
+//   if (response === null) {
+//     return {
+//       redirect: {
+//         destination: '/404',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {
-      project: JSON.parse(JSON.stringify(response)),
-    },
-  };
-};
+//   return {
+//     props: {
+//       project: JSON.parse(JSON.stringify(response)),
+//     },
+//   };
+// };
 
 // RY: moved from SSG to SSR since data updated frequently from DB
 // export const getStaticProps: GetStaticProps = async ({ params }) => {
